4aea4e7250893910d917b5c74ed41f086d317b2f
Josep Morey <josepmorey@gmail.com>
2019-11-30 12:10:00 +0100

    gateway

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "hotels", url: "http://localhost:4003/graphql" },
+    { name: "gatro", url: "http://localhost:4002/graphql" },
+    { name: "destinations", url: "http://localhost:4001/graphql" }
+  ]
+});
+
+(async () => {
+  const { schema, executor } = await gateway.load();
+
+  const server = new ApolloServer({ schema, executor });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
--- services/hotels/src/main.ts
+++ services/hotels/src/main.ts
@@ -44,6 +44,6 @@ const server = new ApolloServer({
   ])
 });
 
-server.listen({ port: 4004 }).then(({ url }) => {
+server.listen({ port: 4003 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
